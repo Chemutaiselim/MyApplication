527c7842587fee9b10233a26ec7ef0ff0bd980fd
Selim <selimchemutai19@gmail.com>
2022-01-19 14:10:18 +0300

    Commit

--- app/src/main/java/com/pamela/myapplication/MainActivity.kt
+++ app/src/main/java/com/pamela/myapplication/MainActivity.kt
@@ -2,10 +2,14 @@ package com.pamela.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pamela.myapplication.databinding.ActivityMainBinding
+lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
