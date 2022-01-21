d8166ba91db1518d3e5d588bc3bc7372c6bc2d3f
Selim <selimchemutai19@gmail.com>
2022-01-21 15:31:44 +0300

    Commit

--- app/src/main/java/com/pamela/myapplication/MainActivity.kt
+++ app/src/main/java/com/pamela/myapplication/MainActivity.kt
@@ -3,6 +3,9 @@ package com.pamela.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.pamela.myapplication.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
+
 lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +16,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         //calculate the tip
+        binding.calculateButton.setOnClickListener { calculateTip() }
 
     }
+
+    private fun calculateTip() {
+        //Store the cost of service from edit text
+        val stringInTextField = binding.costOfService.text.toString()
+        //.text references the text in the UI
+        //convert the acquired text to double.
+       val cost = stringInTextField.toDouble()
+
+        //Checked Radio Button
+
+        //assign the options to integers
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        //Calculate the tip and roundup
+        var tip = tipPercentage * cost
+        //the switch
+        val roundUp = binding.roundUpSwitch.isChecked
+        //if the roundUp statement is true;
+        if (roundUp){
+            //The ceil function can round up the tip
+            tip=ceil(tip)
+        }
+
+        //convert to required currency
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text= getString(R.string.tip_amount, formattedTip)
+    }
 }
+
