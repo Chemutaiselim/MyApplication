44830083813f85ec49bab209757989ff144c21b2
Selim <selimchemutai19@gmail.com>
2022-01-31 22:04:20 +0300

    MainActivity.kt change keyboard after pressing enter

--- app/src/main/java/com/pamela/myapplication/MainActivity.kt
+++ app/src/main/java/com/pamela/myapplication/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.pamela.myapplication
 
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import com.pamela.myapplication.databinding.ActivityMainBinding
 import java.text.NumberFormat
@@ -19,6 +23,8 @@ class MainActivity : AppCompatActivity() {
 
         //calculate the tip
         binding.calculateButton.setOnClickListener { calculateTip() }
+
+        binding.costOfServiceEditText.setOnKeyListener {view,keyCode,_-> handleKeyEvent(view, keyCode)}
 
     }
 
@@ -73,6 +79,17 @@ class MainActivity : AppCompatActivity() {
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
         //Show Result
         //Log.v(TAG,"FORMATTED TIP{$formattedTip}")
+    }
+
+    //function to hide onscreen keyboard after pressing enter
+    private fun handleKeyEvent(view: View, keyCode: Int):Boolean{
+        if (keyCode==KeyEvent.KEYCODE_ENTER){
+            //hide the keyboard
+            val inputMethodManager= getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
 
